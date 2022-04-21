18db0ead9f025c8442ee2d9d0d1da3926e409a7c
rushikesh <dikeyrushikesh@gmail.com>
2022-04-21 23:20:45 +0530

    started with day 19th

--- day_19/main.py
+++ day_19/main.py
@@ -3,13 +3,47 @@
 # main project code at end
 
 # Author: Rushikesh Dikey
-# Date: 14-04-2022
+# Date: 21-04-2022
 
 
+from turtle import Turtle, Screen
 
-Started with some revison
 
-continuing DAY 3
+# calling class
+tom = Turtle()
+
+
+def move_forward():
+    tom.forward(10)
+
+
+def move_backward():
+    tom.backward(10)
+
+
+def move_left():
+    new_heading = tom.heading() + 10
+    tom.setheading(new_heading)
+
+
+def move_right():
+    new_heading = tom.heading() - 10
+    tom.setheading(new_heading)
+
+
+def clear():
+    tom.clear()
+
+
+screen = Screen()
+screen.listen()
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(move_left, "a")
+screen.onkey(move_right, "d")
+screen.onkey(clear, "c")
+screen.exitonclick()
+
 
 
 
