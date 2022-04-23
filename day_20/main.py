22c59efe63836d36c7bdefa3c7c6f762fb052d78
rushikesh <dikeyrushikesh@gmail.com>
2022-04-23 23:13:08 +0530

    updated the README.md for day 20 code

---
+++ day_20/main.py
@@ -0,0 +1,9 @@
+# # start to python day-20 code
+# # starting code is basics learned
+# # main project code at end
+#
+# # Author: Rushikesh Dikey
+# # Date: 24-04-2022
+#
+#
+# from turtle import Turtle, Screen
